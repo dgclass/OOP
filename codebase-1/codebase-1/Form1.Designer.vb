933d88092f4c6a5aaafa4da315344a16845ed435
Dominic <drguiritan@urios.edu.ph>
2021-09-19 09:47:12 +0800

    new

--- codebase-1/codebase-1/Form1.Designer.vb
+++ codebase-1/codebase-1/Form1.Designer.vb
@@ -39,7 +39,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(460, 165)
+        Me.ClientSize = New System.Drawing.Size(453, 169)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
